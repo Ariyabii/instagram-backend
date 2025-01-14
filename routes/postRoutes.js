3e1080c27825cc25b285b7b479ffd2dfa12b7289
24HP1005 24HP1005 <24HP1005@admins-iMac.local>
2025-01-14 02:19:45 -0800

    new

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -5,65 +5,49 @@ const authMiddleware = require("../auth-middleware");
 
 const postRoute = Route();
 
-// postRoute.get("/posts", async (req, res) => {
-//   const posts = await postModel.find().populate("userId", "email username _id");
-//   res.status(200).json(posts);
-// });
+postRoute.get("/posts", async (req, res) => {
+  const posts = await postModel.find().populate("userId", "email username _id");
+  res.status(200).json(posts);
+});
 
-// postRoute.post("/post/create", authMiddleware, async (req, res) => {
-//   try {
-//     const { caption, postImage, userId, profileImage, userName } = req.body;
-//     const createPost = await postModel.create({
-//       caption,
-//       profileImage,
-//       postImage,
-//       userName,
-//       userId,
-//     });
-//     await userModel.findByIdAndUpdate(userId, {
-//       $push: {
-//         posts: createPost._id,
-//       },
-//     });
-//     res.status(200).json(createPost);
-//   } catch (error) {
-//     throw new Error(error);
-//   }
-// });
+postRoute.post("/post/create", authMiddleware, async (req, res) => {
+  try {
+    const { caption, postImage, userId, profileImage, userName } = req.body;
+    const createPost = await postModel.create({
+      caption,
+      profileImage,
+      postImage,
+      userName,
+      userId,
+    });
+    await userModel.findByIdAndUpdate(userId, {
+      $push: {
+        posts: createPost._id,
+      },
+    });
+    res.status(200).json(createPost);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
 
-// postRoute.get("/posts", authMiddleware, async (req, res) => {
-//   try {
-//     const posts = await postModel
-//       .find()
-//       .populate(
-//         "userId",
-//         "username profileImage",
-//         " bio",
-//         " posts",
-//         "following",
-//         "followers"
-//       );
-//     res.json(posts);
-//   } catch (error) {
-//     res.status(404).json({ message: `failed to get posts, ${error}` });
-//   }
-// });
+postRoute.get("/posts", authMiddleware, async (req, res) => {
+  try {
+    const posts = await postModel
+      .find()
+      .populate("userId", "username profileImage");
+    res.json(posts);
+  } catch (error) {
+    res.status(404).json({ message: `failed to get posts, ${error}` });
+  }
+});
 
-// postRoute.get("/posts", async (req, res) => {
-//   const posts = await postModel
-//     .find()
-//     .populate(
-//       "likes",
-//       "username profileImage",
-//       "userId",
-//       "username profileImage",
-//       " bio",
-//       " posts",
-//       "following",
-//       "followers"
-//     );
-//   res.status(200).json(posts);
-// });
+postRoute.get("/posts", async (req, res) => {
+  const posts = await postModel
+    .find()
+    .populate("likes", "username profileImage");
+  res.status(200).json(posts);
+});
 
 postRoute.get("/posts", async (req, res) => {
   const posts = await postModel.find().populate({
